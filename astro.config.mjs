962ec0a1f95a1645f4320f839bb6e8c218092f21
Miracle Jude <judemiraco0@gmail.com>
2022-07-14 21:46:02 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), preact()]
+  integrations: [tailwind(), react()]
 });
